160fcb8a0d6b253bb9dc3b582c23e9b805a63529
BielTeles <devteles@gmail.com>
2025-06-17 15:11:45 -0300

    Atualiza a configuração do Next.js para aplicar o Bundle Analyzer apenas quando solicitado e adiciona a dependência 'cross-env' para facilitar a definição de variáveis de ambiente nos scripts de build.

--- next.config.ts
+++ next.config.ts
@@ -13,4 +13,7 @@ const nextConfig: NextConfig = {
   },
 };
 
-export default withBundleAnalyzer(nextConfig);
+// Aplica o Bundle Analyzer apenas quando solicitado
+export default process.env.ANALYZE === 'true' 
+  ? withBundleAnalyzer(nextConfig) 
+  : nextConfig;
